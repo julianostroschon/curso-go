424bc191b24d20bed79afd4753d2162706ecf52d
julianostroschon <julianostroschon@gmail.com>
2023-11-09 18:47:28 -0300

    ✨ Slices

--- main.go
+++ main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	y := 12.12
-	x := math.Ceil(y)
+	var slices []string
+	integers := [1]int{1}
 
-	fmt.Println(x)
+	slices = []string{"first", "2º", "other"}
+
+	length := len(slices)
+
+	second := slices[1:2]
+
+	slices = append(slices, "3")
+	fmt.Printf("%+v slices\n", slices)
+	slices = slices[:len(slices)-1]
+
+	fmt.Println("Slices")
+	for index, value := range slices {
+		fmt.Printf("index[%+v], -> value %+v\n", index, value)
+	}
+	fmt.Println("Final Slices")
+
+	fmt.Printf("%+v length\n", length)
+	fmt.Printf("%+v integers\n", integers)
+	fmt.Printf("%+v second\n", second)
 
 }
