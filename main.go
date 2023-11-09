21b60333eb07a3cd0b0682d9fe9def0b970e166f
julianostroschon <julianostroschon@gmail.com>
2023-11-09 18:30:05 -0300

    🧐 Exemplo utilização math

--- main.go
+++ main.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"reflect"
-	"strings"
-
-	"github.com/bxcodec/faker"
+	"math"
 )
 
-func init() {
-	fmt.Println("Input a letter...")
-}
-
-func getFirstLetter(word string) string {
-	return string(word[0])
-}
-
 func main() {
-	var input string
-	var err error
+	y := 12.12
+	x := math.Ceil(y)
 
-	fmt.Scanln(&input)
-	for len(input) > 1 {
-		err = errors.New("Error!\nEnter only a letter")
-		fmt.Println(err)
-		fmt.Scanln(&input)
-	}
+	fmt.Println(x)
 
-	firstLetterInput := strings.ToUpper(getFirstLetter(input))
-
-	if firstLetterInput >= "A" && firstLetterInput <= "Z" {
-		for {
-			person, _ := faker.
-				GetPerson().
-				FirstName(reflect.Value{})
-
-			firstLetterInName := getFirstLetter(fmt.Sprintf("%+v", person))
-
-			if firstLetterInName == firstLetterInput {
-				fmt.Println(person)
-				break
-			}
-		}
-	}
 }
